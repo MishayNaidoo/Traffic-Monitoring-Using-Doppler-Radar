05da99932c0c4b4dc42bb88900c84d144bd90cfe
MishayNaidoo <naidoo.mishay@gmail.com>
2024-05-21 16:38:36 +0200

    Added the DC power supply data from Radar lab to the HumanWalkingData folder

---
+++ ADC/SPI_FullDuplex_ComDMA/STM32CubeIDE/Debug/Example/User/subdir.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+E:/UCT/Masters/Traffic-Monitoring-Using-Doppler-Radar/ADC/SPI_FullDuplex_ComDMA/Src/main.c \
+E:/UCT/Masters/Traffic-Monitoring-Using-Doppler-Radar/ADC/SPI_FullDuplex_ComDMA/Src/stm32f4xx_hal_msp.c \
+E:/UCT/Masters/Traffic-Monitoring-Using-Doppler-Radar/ADC/SPI_FullDuplex_ComDMA/Src/stm32f4xx_it.c \
+../Example/User/syscalls.c \
+../Example/User/sysmem.c 
+
+OBJS += \
+./Example/User/main.o \
+./Example/User/stm32f4xx_hal_msp.o \
+./Example/User/stm32f4xx_it.o \
+./Example/User/syscalls.o \
+./Example/User/sysmem.o 
+
+C_DEPS += \
+./Example/User/main.d \
+./Example/User/stm32f4xx_hal_msp.d \
+./Example/User/stm32f4xx_it.d \
+./Example/User/syscalls.d \
+./Example/User/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Example/User/main.o: E:/UCT/Masters/Traffic-Monitoring-Using-Doppler-Radar/ADC/SPI_FullDuplex_ComDMA/Src/main.c Example/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_STM32F4_DISCO -DSTM32F407xx -DUSE_HAL_DRIVER -c -I../../Inc -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/BSP/STM32F4-Discovery -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Example/User/stm32f4xx_hal_msp.o: E:/UCT/Masters/Traffic-Monitoring-Using-Doppler-Radar/ADC/SPI_FullDuplex_ComDMA/Src/stm32f4xx_hal_msp.c Example/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_STM32F4_DISCO -DSTM32F407xx -DUSE_HAL_DRIVER -c -I../../Inc -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/BSP/STM32F4-Discovery -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Example/User/stm32f4xx_it.o: E:/UCT/Masters/Traffic-Monitoring-Using-Doppler-Radar/ADC/SPI_FullDuplex_ComDMA/Src/stm32f4xx_it.c Example/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_STM32F4_DISCO -DSTM32F407xx -DUSE_HAL_DRIVER -c -I../../Inc -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/BSP/STM32F4-Discovery -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Example/User/%.o Example/User/%.su: ../Example/User/%.c Example/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_STM32F4_DISCO -DSTM32F407xx -DUSE_HAL_DRIVER -c -I../../Inc -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/BSP/STM32F4-Discovery -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Example-2f-User
+
+clean-Example-2f-User:
+	-$(RM) ./Example/User/main.d ./Example/User/main.o ./Example/User/main.su ./Example/User/stm32f4xx_hal_msp.d ./Example/User/stm32f4xx_hal_msp.o ./Example/User/stm32f4xx_hal_msp.su ./Example/User/stm32f4xx_it.d ./Example/User/stm32f4xx_it.o ./Example/User/stm32f4xx_it.su ./Example/User/syscalls.d ./Example/User/syscalls.o ./Example/User/syscalls.su ./Example/User/sysmem.d ./Example/User/sysmem.o ./Example/User/sysmem.su
+
+.PHONY: clean-Example-2f-User
+
